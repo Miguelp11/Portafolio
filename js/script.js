0aecabf94b190dd1fd9536a673d7f3f8c6b9f2eb
Miguel Ponce Local <miguel_p1194@hotmail.com>
2021-05-13 17:36:46 -0500

    Se agrega el proyecto flappybird y se le añade sombras a los items de habilidades

--- js/script.js
+++ js/script.js
@@ -32,12 +32,15 @@ var typed = new Typed('.animado', {
 const modal= document.getElementById('modal-portafolio');
 const modal1= document.getElementById('modal-portafolio1');
 const modal2 =document.getElementById('modal-portafolio2')
+const modal3 =document.getElementById('modal-portafolio3')
 const botonModal= document.getElementById('cerrar-modal');
 const botonModal1= document.getElementById('cerrar-modal1');
 const botonModal2= document.getElementById('cerrar-modal2');
+const botonModal3= document.getElementById('cerrar-modal3');
 const proyecto1= document.getElementById('proyecto1');
 const proyecto2= document.getElementById('proyecto2');
 const proyecto3 =document.getElementById('proyecto3');
+const proyecto4 =document.getElementById('proyecto4');
 
 proyecto1.addEventListener('click', ()=>{
     modal.classList.add('abrir-modal');
@@ -48,6 +51,9 @@ proyecto2.addEventListener('click', ()=>{
 proyecto3.addEventListener('click', ()=>{
     modal2.classList.add('abrir-modal');
 });
+proyecto4.addEventListener('click', ()=>{
+    modal3.classList.add('abrir-modal');
+});
 botonModal.addEventListener('click',()=>{
     modal.classList.toggle('abrir-modal');
 });
@@ -56,4 +62,7 @@ botonModal1.addEventListener('click',()=>{
 });
 botonModal2.addEventListener('click',()=>{
     modal2.classList.toggle('abrir-modal');
+});
+botonModal3.addEventListener('click',()=>{
+    modal3.classList.toggle('abrir-modal');
 });
